8d44420493e464e8a4bafb486ce844d8e4c56082
Ehacko <49614514+Ehacko@users.noreply.github.com>
2022-11-27 22:00:41 -0400

    a

--- script/script.js
+++ script/script.js
@@ -197,7 +197,7 @@ function turn(){
         case (["range","attack","switch"].some(str=>event.currentTarget.classList.contains(str))):
             if(event.currentTarget.classList.contains("attack")) event.currentTarget.removeChild(event.currentTarget.children[1]);
             if(event.currentTarget.classList.contains("switch")) s.parentElement.append(event.currentTarget.children[1]);
-            if(pawnMoves[s.classList[1]].attack?.type == "ranged" && !event.currentTarget.classList.contains("attack")) event.currentTarget.append(s);
+            if(!pawnMoves[s.classList[1]].attack?.type == "ranged" && !event.currentTarget.classList.contains("attack")) event.currentTarget.append(s);
             refresh()
             next_turn()
             break;
